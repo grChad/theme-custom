26e1e6c828e3fe052b4080480b20301dadc42437
grChad <bgos-jesus@hotmail.com>
2024-10-07 12:25:14 -0500

    feat: highlight groups rainbow

--- lua/theme-nvim/integrations/indent_bl.lua
+++ lua/theme-nvim/integrations/indent_bl.lua
@@ -1,6 +1,19 @@
-local cp = require('theme-nvim.palletes.frappe')
-
 return {
-   IndentBlanklineChar = { fg = cp.surface0 },
-   IndentBlanklineContextChar = { fg = cp.text },
+	-- Indenthightlight
+	Ibl_RainbowRed = { fg = '#784E50' },
+	Ibl_RainbowYellow = { fg = '#7D6D4D' },
+	Ibl_RainbowBlue = { fg = '#385B74' },
+	Ibl_RainbowOrange = { fg = '#615350' },
+	Ibl_RainbowGreen = { fg = '#4F6640' },
+	Ibl_RainbowViolet = { fg = '#5E4874' },
+	Ibl_RainbowCyan = { fg = '#3C5B6C' },
+
+	-- Scopes Highlight
+	Ibl_RainbowRedScope = { fg = '#EA999C' },
+	Ibl_RainbowYellowScope = { fg = '#E5C890' },
+	Ibl_RainbowBlueScope = { fg = '#7DC8FC' },
+	Ibl_RainbowOrangeScope = { fg = '#D19A66' },
+	Ibl_RainbowGreenScope = { fg = '#B2E292' },
+	Ibl_RainbowVioletScope = { fg = '#C678DD' },
+	Ibl_RainbowCyanScope = { fg = '#56B6C2' },
 }
